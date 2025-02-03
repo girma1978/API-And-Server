9a6831ba02ad57a1e4ebc4a79d10d9393dd280b7
Lakulength@1717 <girm2002@gmail.com>
2025-02-03 11:10:19 -0500

    routes modified further

--- server/src/routes/htmlRoutes.ts
+++ server/src/routes/htmlRoutes.ts
@@ -1,13 +1,29 @@
+
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
+import express from 'express';  // Import Express
 import { Router } from 'express';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+const app = express();  // Create an Express app instance
 const router = Router();
 
-// TODO: Define route to serve index.html
-router.get('/', (_, res) => {
-  res.sendFile(path.join(__dirname, 'path_to_your_directory', 'index.html'));
+// Serve static assets from the client build directory
+router.use(express.static(path.join(__dirname, '../../../client/dist')));
+
+// Serve the index.html for all other requests
+router.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, '../../../client/dist/index.html'));
+});
+
+// Use the router in the app
+app.use(router);
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
 });
 
 export default router;
